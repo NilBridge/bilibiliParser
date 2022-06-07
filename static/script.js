7d3138635143c46209f2ff075c35293e2abfa109
Lition802 <2959435045@qq.com>
2022-06-07 11:26:53 +0800

    ok

--- static/script.js
+++ static/script.js
@@ -2,6 +2,43 @@ function parseDownloadUrl(bvid) {
     return axios(`http://mc.lition.online:3999/download/${bvid}`);
 }
 
+var prompt = function (message, style, time)
+{
+    style = (style === undefined) ? 'alert-success' : style;
+    time = (time === undefined) ? 1200 : time * 1000;
+    $('<div>')
+        .appendTo('body')
+        .addClass('alert ' + style)
+        .html(message)
+        .show()
+        .delay(time)
+        .fadeOut();
+};
+
+// 成功提示
+var success_prompt = function(message, time)
+{
+    prompt(message, 'alert-success', time);
+};
+
+// 失败提示
+var fail_prompt = function(message, time)
+{
+    prompt(message, 'alert-danger', time);
+};
+
+// 提醒
+var warning_prompt = function(message, time)
+{
+    prompt(message, 'alert-warning', time);
+};
+
+// 信息提示
+var info_prompt = function(message, time)
+{
+    prompt(message, 'alert-info', time);
+};
+
 function onClick() {
     let bv = document.getElementById("input-bv").value;
     if (bv == "") {
@@ -21,7 +58,7 @@ function onClick() {
                 tg.error("无法找到视频解析  ::>_<::");
                 return;
             }
-            window.open(dt.data.url[0].url)
+            info_prompt(dt.data.url[0].url,10);
         }
     ).catch(alert)
 }
